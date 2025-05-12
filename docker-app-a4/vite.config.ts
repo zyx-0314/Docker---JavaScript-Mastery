b1bb758e4c2163aab977dc864d35f0213cc1a7a5
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-12 17:31:58 +0800

    feat: Implement Docker Compose
    - added initialized docker
    - modify test

--- docker-app-a4/vite.config.ts
+++ docker-app-a4/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,    // force polling
+      interval: 300,       // check every 300ms
+    },
+    host: true,
+    strictPort: true,
+  },
 })
